a1cfc8b6ffd01e08a1bf01cd12e2d9706e6f5484
evers <evers@DESKTOP-575M1E3>
2022-06-12 22:00:29 -0300

    Create connection

---
+++ src/java/br/com/sorvetesbark/factory/ConnectionFactory.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.sorvetesbark.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author evers
+ */
+public class ConnectionFactory {
+    
+    public static final String conn = "jdbc:mysql://localhost:3306/bark-database?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    public static final String user = "root";
+    public static final String password = "";
+    public static final String driver = "com.mysql.jdbc.Driver";
+       
+    
+    public static Connection getConnection(){
+    
+        try {
+            Class.forName(driver);
+            return DriverManager.getConnection(conn, user, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw  new RuntimeException("Erro na conexão: ", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeConnection(Connection conn, PreparedStatement stmt) {
+
+        closeConnection(conn);
+
+        try {
+
+            if (stmt != null) {
+                stmt.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeConnection(Connection conn, PreparedStatement stmt, ResultSet rs) {
+
+        closeConnection(conn, stmt);
+
+        try {
+
+            if (rs != null) {
+                rs.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+    
+}
---
+++ src/java/br/com/sorvetesbark/model/Usuario.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.sorvetesbark.model;
+
+/**
+ *
+ * @author evers
+ */
+public class Usuario {
+    
+    private int id;
+    private String user;
+    private String nome;
+    private String nascimento;
+    private String cpf;
+    private Boolean status;
+    private String password;   
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the nascimento
+     */
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    /**
+     * @param nascimento the nascimento to set
+     */
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the status
+     */
+    public Boolean getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+        
+    
+}
